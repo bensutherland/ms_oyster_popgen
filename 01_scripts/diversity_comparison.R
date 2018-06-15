1df6927d4806a1b0d2984f5507a687aa9a2d01af
bensutherland <bensutherland7@gmail.com>
2018-06-15 13:51:39 -0700

    Add pi corr figure and export figs to pdf

--- 01_scripts/diversity_comparison.R
+++ 01_scripts/diversity_comparison.R
@@ -17,30 +17,48 @@ head(w)
 
 # Explore data
 summary(h$PI)
-mean(h$PI)
 summary(wf$PI)
-mean(wf$PI)
 summary(w$PI)
-mean(w$PI)
 
 # Merge
 x <- merge(h, wf, by = c("CHROM","POS"))
 x <- merge(x, w, by = c("CHROM","POS"))
-colnames(x)[3] <- "PI.h"
-colnames(x)[4] <- "PI.wf"
-colnames(x)[5] <- "PI.w"
+colnames(x)[3] <- "Pi.hatchery"
+colnames(x)[4] <- "Pi.wild-farm"
+colnames(x)[5] <- "Pi.wild"
 
 head(x)
-par(mfrow=c(3,1))
-plot(x$PI.h, ylab = "Pi (hatchery)", ylim = c(0,1))
-plot(x$PI.wf, ylab = "Pi (wild-to-farm)", ylim = c(0,1))
-plot(x$PI.w, ylab = "Pi (wild)", ylim = c(0,1))
 
-# How correlated are these values?
-cor.test(h$PI, w$PI) # highly correlated
-cor.test(wf$PI, w$PI) # very highly correlated
+#### 1.1. Pi per locus across types ####
 
-# Model
+pdf(file=paste("Pi_per_locus_by_type.pdf",sep=""), height=5, width=7)
+par(mfrow=c(1,3))
+plot(x$Pi.hatchery, ylab = "Pi (hatchery)", xlab = "Locus.index", ylim = c(0,1), las = 1)
+plot(x$`Pi.wild-farm`, ylab = "Pi (wild-to-farm)", xlab = "Locus.index", ylim = c(0,1), las = 1)
+plot(x$Pi.wild, ylab = "Pi (wild)", xlab = "Locus.index", ylim = c(0,1), las = 1)
+dev.off()
+
+#### 1.2 Pi correlation ####
+# How correlated are the pi values across loci for the different types (hatchery, wild and wild-farm)?
+#install.packages('corrplot')
+library('corrplot')
+cor.test(h$PI, w$PI) # correlated (cor = 0.52)
+cor.test(h$PI, wf$PI) # correlated (cor = 0.48)
+cor.test(wf$PI, w$PI) # highly correlated (cor = 0.84)
+
+cor.set <- cor(x[,c("Pi.hatchery","Pi.wild-farm","Pi.wild")]
+               , use = "pairwise.complete.obs" # cor bw ea pair var is comp using all compl pairs of obs on those var # note: only works with "pearson" method    
+)
+
+
+# Plot correlation
+pdf(file=paste("corr_of_pi_bw_groups.pdf",sep=""), height=5, width=5)
+par(mfrow=c(1,1), mar = c(3,3,4,5))
+corrplot(cor.set, method="number", addshade="all", tl.col = "black", tl.cex=0.8, type = "lower", diag = T, tl.pos = "lt", cl.pos = "r")
+corrplot(cor.set, method="circle", addshade="all", tl.col = "black", tl.cex=0.8, type = "upper", add = T, tl.pos = "n")
+dev.off()
+
+#### 1.3. Pi differences across groups (model) ####
 dim(h)
 dim(wf)
 dim(w)
@@ -55,26 +73,28 @@ data <- cbind(all,type)
 head(data)
 tail(data)
 
-par(mfrow=c(1,1))
+par(mfrow=c(1,1), mar = c(4,4,4,4))
 mod1 <- aov(data$PI ~ data$type)
 
 boxplot(data$PI ~ data$type, las = 1, ylab = "Pi", ylim = c(0,0.6))
-text(paste("avg=", round(mean(x$PI.h), digits = 3)), x = 1, y = 0.45)
-text(paste("avg=", round(mean(x$PI.w), digits = 3)), x = 2, y = 0.45)
-text(paste("avg=", round(mean(x$PI.wf), digits = 3)), x = 3, y = 0.45)
+text(paste("avg=", round(mean(x$Pi.hatchery, na.rm = T), digits = 3)), x = 1, y = 0.45)
+text(paste("avg=", round(mean(x$Pi.wild, na.rm = T), digits = 3)), x = 2, y = 0.45)
+text(paste("avg=", round(mean(x$`Pi.wild-farm`, na.rm = T), digits = 3)), x = 3, y = 0.45)
 summary(mod1)
 TukeyHSD(mod1)
 text(x = 1, y = 0.55, labels = "a")
 text(x = 2, y = 0.55, labels = "b")
-text(x = 3, y = 0.55, labels = "ab")
+text(x = 3, y = 0.55, labels = "b")
 #text(paste("pval=", round(summary(mod1)[[1]][["Pr(>F)"]][1], digits = 3)), x = 2, y = 0.42)
 
 
 #### 2. Heterozygosity ####
 het <- read.table("batch_1.het", header = T, row.names = 1)
 head(het)
+
+pdf(file=paste("Fis_per_indiv.pdf",sep=""), height=5, width=14)
 par(mar=c(6,5,3,3), mfrow=c(1,1))
 plot(het$F, xaxt ="n", xlab = "", ylab = "Fis")
 axis(side = 1, labels = rownames(het), at = c(1:nrow(het)), las = 2, cex.axis = 0.5)
-
+dev.off()
 
