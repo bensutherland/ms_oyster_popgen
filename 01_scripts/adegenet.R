7e1e4e3df3debea8f458fcc64c5e3c9614aef6a5
bensutherland <bensutherland7@gmail.com>
2018-07-05 15:36:57 -0700

    Collect Fst values in table, heatmap, and remove indiv pop comparisons to own script

--- 01_scripts/adegenet.R
+++ 01_scripts/adegenet.R
@@ -34,6 +34,7 @@ library("adegenet")
 sessionInfo()
 
 #### 1. Import Genomics (genlight) ####
+# The following was produced by plink out of stacks (using .ped and .map)
 my.data <- read.PLINK(file = "06-stacks_rx/batch_1.raw")
 # my.data <- read.PLINK(file = "07-filtered_vcfs/batch_1_0.5Mreads_r70_p7_maf0.05.raw")
 my.data
@@ -236,203 +237,32 @@ pairwise.WCfst(all.data.hf)
 boot.fst.all <- boot.ppfst(dat = all.data.hf, nboot = 100, quant = c(0.025,0.975))
 boot.fst.all
 
-write.csv(boot.fst.all$ll, file = "boot_fst_all_ll.csv")
-write.csv(boot.fst.all$ul, file = "boot_fst_all_ul.csv")
+# Collect output
+lower.limit <- t(boot.fst.all$ll)
+upper.limit <- boot.fst.all$ul
+upper.limit[is.na(upper.limit)] <- 0
+lower.limit[is.na(lower.limit)] <- 0
+boot.fst.all.output <- upper.limit + lower.limit
+boot.fst.all.output
 
-#### 7. Investigate Wild Populations Only ####
-sep.obj <- seppop(x = my.data.gid)
-names(sep.obj)
+write.csv(x = boot.fst.all.output, file = "11-other_stats/boot.fst.output.csv")
 
-# Wild populations (and moved to farm)
-wild.bc.gid <- repool(sep.obj$Hisnit, sep.obj$Pendrell, sep.obj$PendrellFarm, sep.obj$Pipestem, sep.obj$Serpentine)
-nPop(wild.bc.gid)
-nInd(wild.bc.gid)
-indNames(wild.bc.gid)
+# Heatmap output
+# install.packages("plsgenomics")
+require(plsgenomics)
+matrix.heatmap(mat = boot.fst.all.output)
 
-# Show sample size per population
-par(mfrow=c(1,1), mar=c(8,5,3,3))
-barplot(table(pop(wild.bc.gid)), col=funky(17)
-        #, las=3, las = 1
-        , las=2
-        , xlab=""
-        , ylab="Sample size"
-        , ylim = c(0,40))
-abline(h = c(10,20,30), lty=2)
+require("lattice")
+levelplot(boot.fst.all.output, scales=list(x=list(rot=90)))
 
-# Change from genind file to hfstat
-wild.bc.hf <- genind2hierfstat(wild.bc.gid)
-rownames(wild.bc.hf) <- indNames(wild.bc.gid)
-
-# Perform PCA on a matrix of individual genotype frequencies (hierfstat)
-y <- indpca(wild.bc.hf, ind.labels = rownames(wild.bc.hf))
-plot(y, cex = 0.7)
-
-# Perform dapc on the wild only individuals
-dapc2 <- dapc(wild.bc.gid, n.pca = 10, n.da = 1)
-scatter(dapc2, scree.da = F, bg = "white", legend = T
-        , txt.leg=rownames(dapc2$means)
-        , posi.leg = "topleft"
-        )
-
-# Composition plot (barplot showing the probabilities of assignments of individuals to the different clusters)
-par(mar=c(10,3,3,3))
-compoplot(dapc2
-          #, lab="" # comment out if you want sample labels
-          , txt.leg = rownames(dapc2$means)
-          , posi = "topright"
-          #          , cex = 0.7
-          , cex.names = 0.6 
-)
-
-# Loading plot # Plot marker variance contribution to DAPC
-par(mfrow=c(1,1), mar=c(3,4,3,3))
-
-# Plot the loading values of the different markers into the DAPC
-loadingplot(dapc2$var.contr, thres=1e-3)
-
-# Identify the top loading marker names
-toploading.markers <- dimnames(dapc2$var.contr)[[1]][which(dapc2$var.contr > 0.0005)]
-toploading.markers <- gsub(pattern = "\\..*", replacement = "", toploading.markers) # match the genlight name format (remove .1 or .2)
-toploading.markers
-
-# # subset the gid file w/ toploading markers
-# locNames(wild.bc.gid)
-# 
-# wild.bc.gid.toploading <- wild.bc.gid[, loc=toploading.markers]
-# wild.bc.gid.toploading
+require("gplots")
+heatmap.2(boot.fst.all.output)
 
 
-#### TOPLOADING STUFF ####
-# Subset the original my.data genlight file with the toploading markers and convert back to a genlight file
-my.data.mat <- as.matrix(my.data)
-my.data.mat[1:5,1:5]
-# retain only top loading markers
-my.data.mat.toploading <- my.data.mat[,toploading.markers]
-# retain only wild samples (removing DeepBay only currently)
-samples.to.keep <- rownames(my.data.mat)[grep(pattern = "DeepBay", x = rownames(my.data.mat), invert = T)]
-my.data.mat.toploading.wild <- my.data.mat.toploading[samples.to.keep,]
-
-# Convert back to genlight format
-my.data.toploading.gl  <- as.genlight(my.data.mat.toploading.wild)
-
-# Determine distance between individuals
-# First use seploc to create a list of ten blocks of loci, each to be computed in parallel
-x <- seploc(my.data.toploading.gl, n.block=10, parallel=F)
-
-## Use dist in a lapply loop to compute pairwise distances between individuals for each block
-lD <- lapply(x, function(e) dist(as.matrix(e)))
-
-# Obtain general distance matrix by summing these
-D <- Reduce("+", lD)
-
-# Plot a neighbor-joining tree using ape's nj function on the distance matrix
-plot(nj(D), type="fan", cex=0.7)
-# this fails if there are too few markers
-# plot(njs(D), type="fan", cex=0.7)
-
-# Pairwise Fst
-pairwise.WCfst(wild.bc.hf)
-
-# Bootstrapping
-# requires latest hierfstat (v0.04-29) otherwise get error
-library(devtools)
-install_github("jgx65/hierfstat")
-library("hierfstat")
-boot.fst.wild.bc <- boot.ppfst(dat = wild.bc.hf, nboot = 100, quant = c(0.025,0.975))
-boot.fst.wild.bc
+save.image(file = "11-other_stats/adegenet_output.RData") # Save out this data
 
 
-#### 8. BC Seq Facility (Hisnit, Pendrell) ####
-bc.gid <- repool(sep.obj$Pendrell, sep.obj$Hisnit)
-# could also use # sep.obj$Serpentine
-nPop(bc.gid)
-nInd(bc.gid)
-indNames(bc.gid)
-pop(bc.gid)
 
-# If you want to edit the population variable, do it here
-indiv.used <- as.numeric(gsub(pattern = "*.*\\_", replacement = "", indNames(bc.gid)))
-indiv.used <- as.data.frame(indiv.used)
-indNames(bc.gid)
-refined.pop <- c(rep("Pen_2", times = 15), rep("Pen_3", times = 13), rep("His_3", times = 5), rep("His_6", times = 11)
-                 , rep("His_3", times = 5))
-# TODO # import sample database for auto matching of this using a merge function w/ sample ID and size class
-
-length(indNames(bc.gid))
-length(refined.pop)
-
-pop(bc.gid) <- refined.pop
-pop(bc.gid)
-
-# DAPC
-dapc3 <- dapc(bc.gid, n.pca = 10, n.da = 1)
-scatter(dapc3, scree.da = F, bg = "white", legend = T
-        , txt.leg=rownames(dapc3$means)
-        , posi.leg = "topleft"
-)
-# var (proportion of conserved variance): 0.244
-
-
-# Composition plot (barplot showing the probabilities of assignments of individuals to the different clusters)
-par(mar=c(10,3,3,3))
-compoplot(dapc3
-          #, lab="" # comment out if you want sample labels
-          , txt.leg = rownames(dapc3$means)
-          , posi = "topright"
-          #          , cex = 0.7
-          , cex.names = 0.6 
-)
-
-# Loading plot # Plot marker variance contribution to DAPC
-par(mfrow=c(1,1), mar=c(3,4,3,3))
-
-# Plot the loading values of the different markers into the DAPC
-loadingplot(dapc3$var.contr, thres=1e-3)
-
-# Show sample size per population
-par(mfrow=c(1,1), mar=c(8,5,3,3))
-barplot(table(pop(bc.gid)), col=funky(17)
-        #, las=3, las = 1
-        , las=2
-        , xlab=""
-        , ylab="Sample size"
-        , ylim = c(0,40))
-abline(h = c(10,20,30), lty=2)
-
-
-# Change to hierfstat
-bc.hf <- genind2hierfstat(bc.gid)
-rownames(bc.hf) <- indNames(bc.gid)
-
-# PCA
-z <- indpca(bc.hf, ind.labels = rownames(bc.hf))
-# z <- indpca(bc.hf, ind.labels = rownames(refined.pop)) # use this if want to see the refined pop
-# plot PCA
-plot(z, cex = 0.7)
-
-
-# Basic stats (gives per loc and overall, but only one value, not one for each comparison)
-bc.stats <- basic.stats(bc.hf, diploid = T, digits = 4)
-
-par(mar=c(5,5,2.5,3))
-plot(bc.stats$perloc$Fst, ylab = "Fst", las = 1) # Plot Fst
-text(x = 2000, y = 0.2, labels = paste(nLoc(my.data),"loci" ))
-text(x = 2000, y = 0.19, labels = paste(nInd(my.data),"ind" ))
-text(x = 2000, y = 0.18, labels = "Size class, Hisnit-Serp")
-text(x = 2000, y = 0.17, labels = "Max. Fst")
-
-
-# Save out these Fst values per locus to match to location in the genome
-perloc.stats.df <- bc.stats$perloc # save df w/ Ho, Hs, Ht, Dst, Htp, Dstp, Fst, Fstp, Fis, Dest (from hierfstat)
-write.csv(x = perloc.stats.df, file = "../ms_oyster_popgen/perloc.stats.csv")
-# match these to their genomic location and plot in Rscript (#todo)
-
-# Weir-Cockerham
-pairwise.wcfst.bc.hf <- pairwise.WCfst(dat = bc.hf)
-
-# Bootstrapping
-boot.fst.obj <- boot.ppfst(dat = bc.hf, nboot = 1000, quant = c(0.025,0.975))
-boot.fst.obj
 
 
 #### 9. Hobs vs Hexp ####
