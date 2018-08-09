cfd46c24d76dec05e60aebd2b5bead6dec929ee9
bensutherland <bensutherland7@gmail.com>
2018-08-09 14:50:52 -0700

    Update nuc diversity script with all populations and improve script

--- 01_scripts/nuc_diversity.sh
+++ 01_scripts/nuc_diversity.sh
@@ -1,28 +1,68 @@
 #!/bin/bash
+# First obtain a list of samples containing keywords (part 1) then calculate Pi for each of these groups of samples.    
 # Run this script from within the stacks_workflow main repo
 
-# Set the samples to use for the comparisons
-# Here using Hisnit, Pendrell, Pipestem and Serpentine for the BC wild
-vcf-query -l 09-diversity_stats/batch_1.vcf | grep -E 'Hisnit|Pendrell|Pipestem|Serpentine' - | grep -vE 'PendrellFarm' - > 09-diversity_stats/wild_bc_samples.txt
+DIVERSITY_FOLDER="09-diversity_stats"
 
-# Here using the BC wild-to-farm sample
-vcf-query -l 09-diversity_stats/batch_1.vcf | grep -E 'PendrellFarm' - > 09-diversity_stats/bc_wild_to_farm_samples.txt
+# Part 1: Collect sample lists for comparison groups 
+# BC Wild = Hisnit, Pendrell, Pipestem and Serpentine
+vcf-query -l $DIVERSITY_FOLDER/batch_1.vcf |
+    grep -E 'Hisnit|Pendrell|Pipestem|Serpentine' - |
+    grep -vE 'PendrellFarm' - \
+        > $DIVERSITY_FOLDER/bc_wild_samples.txt
 
-# Here using Rosewall Farm
-vcf-query -l 09-diversity_stats/batch_1.vcf | grep -E 'Rosewall' - > 09-diversity_stats/rosewall_samples.txt 
+# BC Wild-to-farm = PendrellFarm 
+vcf-query -l $DIVERSITY_FOLDER/batch_1.vcf | 
+    grep -E 'PendrellFarm' - \
+        > $DIVERSITY_FOLDER/bc_wild_to_farm_samples.txt
 
-# Here for France
-vcf-query -l 09-diversity_stats/batch_1.vcf | grep -E 'FranceW' - > 09-diversity_stats/france_wild_samples.txt
-vcf-query -l 09-diversity_stats/batch_1.vcf | grep -E 'FranceC' - > 09-diversity_stats/france_cultured_samples.txt
+# BC Farm 1 = Rosewall 
+vcf-query -l $DIVERSITY_FOLDER/batch_1.vcf | 
+    grep -E 'Rosewall' - \
+        > $DIVERSITY_FOLDER/rosewall_samples.txt 
 
-# Here for China (altogether)
-vcf-query -l 09-diversity_stats/batch_1.vcf | grep -E 'China' - > 09-diversity_stats/china_samples.txt
+# BC Farm 2 = DeepBay
+vcf-query -l $DIVERSITY_FOLDER/batch_1.vcf | 
+    grep -E 'DeepBay' - \
+        > $DIVERSITY_FOLDER/deepbay_samples.txt 
 
-# Here for Guernsey
-vcf-query -l 09-diversity_stats/batch_1.vcf | grep -E 'Guernsey' - > 09-diversity_stats/guernsey_samples.txt
+# France Wild = FranceW
+vcf-query -l $DIVERSITY_FOLDER/batch_1.vcf | 
+    grep -E 'FranceW' - \
+        > $DIVERSITY_FOLDER/france_wild_samples.txt
+
+# France Wild-to-farm
+vcf-query -l $DIVERSITY_FOLDER/batch_1.vcf | 
+    grep -E 'FranceC' - \
+        > $DIVERSITY_FOLDER/france_wild_to_farm_samples.txt
+
+# China Farm 1
+vcf-query -l $DIVERSITY_FOLDER/batch_1.vcf | 
+    grep -E 'ChinaQDC' - \
+        > $DIVERSITY_FOLDER/chinaQDC_samples.txt
+
+# China Farm 2
+vcf-query -l $DIVERSITY_FOLDER/batch_1.vcf | 
+    grep -E 'ChinaRSC' - \
+        > $DIVERSITY_FOLDER/chinaRSC_samples.txt
+
+# China Wild 
+vcf-query -l $DIVERSITY_FOLDER/batch_1.vcf | 
+    grep -E 'ChinaBe|ChinaQDW' - \
+        > $DIVERSITY_FOLDER/china_wild_samples.txt
+
+# Guernsey Farm
+vcf-query -l $DIVERSITY_FOLDER/batch_1.vcf | 
+    grep -E 'Guernsey' - \
+        > $DIVERSITY_FOLDER/guernsey_samples.txt
+
+# Japan
+vcf-query -l $DIVERSITY_FOLDER/batch_1.vcf | 
+    grep -E 'Japan' - \
+        > $DIVERSITY_FOLDER/japan_samples.txt
 
 
-# And Loop
+# Part 2. Loop across the different 'samples' list files and calculate site pi
 ls -1 09-diversity_stats/*_samples.txt |
         perl -pe 's/\.txt//' |
         while read i
