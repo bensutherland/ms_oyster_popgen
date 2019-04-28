d700d28f48275be4a04b79dad588b2e5f8dfeaa7
bensutherland <bensutherland7@gmail.com>
2019-04-28 09:34:32 -0700

    Finalize hard-coded runall outputs

--- 01_scripts/runall_outputs.sh
+++ 01_scripts/runall_outputs.sh
@@ -3,35 +3,35 @@
 
 # 1. Identify loci out of HWE
 echo "Identifying loci out of HWE"
-# populations --in-path 05-stacks --popmap 01-info_files/population_map_retained.txt \
-#         --out-path 07-filtered_vcfs -t 8 \
-#         -r 0.7 --min-populations 15 --min-maf 0.01 \
-#         --hwe 
+populations --in-path 05-stacks --popmap 01-info_files/population_map_retained.txt \
+        --out-path 07-filtered_vcfs -t 8 \
+        -r 0.7 --min-populations 15 --min-maf 0.01 \
+        --hwe 
 
 
 # Make file of loci out of HWE
 echo "Making list of markers out of HWE"
-# awk -F"\t" ' $20 < 0.05 { print $1 "\t" $20 }' 07-filtered_vcfs/populations.sumstats.tsv  |\
-#         grep -vE '^#' - > 07-filtered_vcfs/loci_out_of_hwe.txt
-# awk -F"\t" '{ print $1 }' 07-filtered_vcfs/loci_out_of_hwe.txt | uniq > 07-filtered_vcfs/loci_out_of_hwe_blacklist.txt
+awk -F"\t" ' $20 < 0.05 { print $1 "\t" $20 }' 07-filtered_vcfs/populations.sumstats.tsv  |\
+        grep -vE '^#' - > 07-filtered_vcfs/loci_out_of_hwe.txt
+awk -F"\t" '{ print $1 }' 07-filtered_vcfs/loci_out_of_hwe.txt | uniq > 07-filtered_vcfs/loci_out_of_hwe_blacklist.txt
 echo "List name: 07-filtered_vcfs/loci_out_of_hwe_blacklist.txt ; use this as blacklist"
 
 # 2. Filter and output loci for population genetic analysis (single SNP)
 echo "Filtering and outputing single SNP per locus using blacklist"
-# populations --in-path 05-stacks --popmap 01-info_files/population_map_retained.txt \
-#         --out-path 07-filtered_vcfs -t 8 \
-#         -r 0.7 --min-populations 15 --min-maf 0.01 \
-#         --vcf --fstats --smooth --plink \
-#         --write-single-snp \
-#         --hwe \
-#         -B 07-filtered_vcfs/loci_out_of_hwe_blacklist.txt
+populations --in-path 05-stacks --popmap 01-info_files/population_map_retained.txt \
+        --out-path 07-filtered_vcfs -t 8 \
+        -r 0.7 --min-populations 15 --min-maf 0.01 \
+        --vcf --fstats --smooth --plink \
+        --write-single-snp \
+        --hwe \
+        -B 07-filtered_vcfs/loci_out_of_hwe_blacklist.txt
 
 echo "Saving single-snp data"
-# mkdir 09-diversity_stats 2>/dev/null 
-# cp 07-filtered_vcfs/populations.snps.vcf 09-diversity_stats
-# 
-# mkdir 11-adegenet_analysis 2>/dev/null
-# cp 07-filtered_vcfs/*plink* 11-adegenet_analysis 
+mkdir 09-diversity_stats 2>/dev/null 
+cp 07-filtered_vcfs/populations.snps.vcf 09-diversity_stats
+
+mkdir 11-adegenet_analysis 2>/dev/null
+cp 07-filtered_vcfs/*plink* 11-adegenet_analysis 
 
 
 # 3. Filter and output loci for population genetic analysis (single SNP)
@@ -48,20 +48,21 @@ mkdir 08-fineRADstructure 2>/dev/null
 cp 07-filtered_vcfs/populations.haps.vcf 08-fineRADstructure
 
 echo "Saving all data to 21_haplotype_results"
-mkdir 21_haplotype_results 2>/dev/null
+mkdir 21-haplotype-results 2>/dev/null
 mv 07-filtered_vcfs/* 21_haplotype_results
-
+ 
 # 4. Filter and output loci for selection analysis (single SNP)
 echo "Filtering and outputting single SNP for selection analysis, no HWE filter"
 populations --in-path 05-stacks --popmap 01-info_files/population_map_retained.txt \
         --out-path 07-filtered_vcfs -t 8 \
         -r 0.7 --min-populations 15 --min-maf 0.01 \
         --vcf --fstats --smooth --plink \
+        --write-single-snp \
         --hwe 
 
 echo "Saving all data to 24-selection"
 mkdir 24-selection 2>/dev/null
-mv 07-filtered_vcf/* 24-selection
+mv 07-filtered_vcfs/* 24-selection
 
 
 echo "Done!"
