11c38dcc938a8b14098271a042dafaaa0cddcd9d
bensutherland <bensutherland7@gmail.com>
2019-11-10 13:11:36 -0800

    Improving relatedness calcuation script

--- 01_scripts/relatedness_new_version.R
+++ 01_scripts/relatedness_new_version.R
@@ -52,15 +52,7 @@ my.data.demerelate <- gl2demerelate(gl = my.data)
 my.data.demerelate[1:5,1:5]
 
 
-#### 02. Relatedness with demerelate ####
-#### Does this work?  ####
-# dem_results <- Demerelate(inputdata = my.data.demerelate[,1:60], value = "Mxy"
-#            , file.output = F, object = T, pairs = 10
-#            #, 
-#            )
-
-
-#### 03. Relatedness with related ####
+#### 02. Relatedness with related ####
 # Info on related input can be obtained here: https://rdrr.io/rforge/related/man/related-package.html
 ## genotype file should have one column of individual identifiers and 
 ## then 2 columns for each locus (one for each allele). No other columns are allowed. 
@@ -68,18 +60,17 @@ my.data.demerelate[1:5,1:5]
 ## The file should NOT contain a header row.
 
 # Convert to related input
-str(my.data.demerelate[1:5,1:5])
+my.data.demerelate[1:5,1:5]
 
-# Remove pop column
+# Remove population column
 my.data.related <- my.data.demerelate[,-2] # drop pop column
-str(my.data.related[1:10,1:10])
+my.data.related[1:5,1:5]
 
 # Convert NA to "0" for related format
 my.data.related[is.na(my.data.related)] <- 0 
-str(my.data.related[1:10,1:10])
 my.data.related[1:5,1:5]
 
-# Save out as text file to be able to read in via readgenotypedata
+# Write out as text file to be able to read in via readgenotypedata
 write.table(x = my.data.related, file = "11-adegenet_analysis/my_data_related.txt"
             , quote = F, sep = "\t", row.names = F, col.names = F)
 
@@ -87,6 +78,45 @@ write.table(x = my.data.related, file = "11-adegenet_analysis/my_data_related.tx
 my.data.related <- readgenotypedata(genotype.data = "11-adegenet_analysis/my_data_related.txt")
 
 names(my.data.related)
+
+### Need to change the names
+my.data.related$gdata[1:5,1:5]
+
+pop_name.df <- my.data.related$gdata$V1
+head(pop_name.df)
+str(pop_name.df)
+pop_name.df <- as.data.frame(pop_name.df, stringsAsFactors = FALSE)
+str(pop_name.df)
+pop_name.df$pop_name <- as.character(pop_name.df$pop_name)
+head(pop_name)
+
+# Split the pop
+temp <- separate(data = pop_name.df, col = pop_name, sep = "_", into = c("pop","ind"))
+
+# Merge with stock code file
+codes <- read.delim(file = "../ms_oyster_popgen/00_archive/pop_codes.txt", header = F, sep = "\t")
+colnames(codes) <- c("pop", "pop_code")
+codes$pop <- as.character(codes$pop)
+str(codes)
+
+head(temp)
+
+# Re-attach this instead of the current ind name
+temp_all.df <- merge(x = temp, y = codes, by = "pop", all.x = T)
+head(temp_all.df)
+
+# make a new vector with the pop code and ind
+temp_all.df$new_ID <- paste0(temp_all.df$pop_code, "_", temp_all.df$ind)
+head(temp_all.df)
+
+# Replace original ID with this new one
+my.data.related$gdata$V1 <- temp_all.df$new_ID
+
+# Note that this is now using the stock code instead of the stock name
+# , as the first two digits only will be retained
+my.data.related$gdata[1:5,1:5]
+
+
 
 # coancestry analysis w/ related
 output <- coancestry(genotype.data = my.data.related$gdata
@@ -98,10 +128,11 @@ output <- coancestry(genotype.data = my.data.related$gdata
 # Save out results
 date <- format(Sys.time(), "%Y-%m-%d")
 save.image(file = paste0(output.dir, "kinship_analysis_", date, ".Rdata"))
-# Load results back up (#to build)
 
 
-
+#### MANUAL LOAD BACK IN DATA #####
+# Load results back up manually
+load(file = "11-adegenet_analysis/kinship_analysis_2019-08-25.Rdata" )
 
 ## Values: 
 # relatedness: df w/ all pairwise est. of relatedness
@@ -112,6 +143,103 @@ save.image(file = paste0(output.dir, "kinship_analysis_", date, ".Rdata"))
 # See what variables are available for relatedness output
 head(output$relatedness, n = 5)
 colnames(output$relatedness)
+
+####
+### Here forward: TAKEN FROM SIMPLE POP STATS ####
+####
+## The results are in the following formats
+# relatedness: df w/ all pairwise est. of relatedness
+# 1. integer for pair #; 2. indiv 1 ID; 3. indiv 2 ID; 4. group assignment; 5-11. relatedness estim.
+# delta7 & delta8: df w/ delta7 & delta8 est. for relatedness estim. that use it
+# inbreeding: df w/ inbreeding est. per indiv., as used in relatedness est (one row per indiv)
+
+# See what variables are available for relatedness output
+head(output$relatedness, n = 5)
+
+# Extract the necessary parts
+print("Extracting relevant sections of output data")
+rel.wang <- output$relatedness[["wang"]]
+rel.ritland <- output$relatedness[["ritland"]]
+rel.quellergt <- output$relatedness[["quellergt"]]
+
+group <- output$relatedness$group
+
+# Make into df
+output.df <- as.data.frame(
+  cbind(  rel.wang
+          , rel.ritland
+          , rel.quellergt
+          , group
+  )
+  , stringsAsFactors = F)
+
+# Format sections of df
+output.df$rel.wang <- as.numeric(output.df$rel.wang)
+output.df$rel.ritland <- as.numeric(output.df$rel.ritland)
+output.df$rel.quellergt <- as.numeric(output.df$rel.quellergt)
+
+str(output.df)
+
+# Make two vectors showing the pops in the contrast
+output.df <- separate(data = output.df, col = group, into = c("pop1", "pop2"), sep = 2, remove = F) # split
+str(output.df)
+
+# compare pop1 and pop2 to identify if same
+output.df$same <- output.df$pop1==output.df$pop2
+
+head(output.df) # e.g. true
+output.df[100:110,] # e.g. false
+dim(output.df)
+
+# Set variables
+same_pops <- TRUE
+datatype <- "SNP"
+
+
+# Are all pop comparisons to be kept, or only same-on-same?
+if(same_pops==TRUE){
+  
+  # Only keep same-on-same comparisons
+  print("Keeping only same-on-same comparisons")
+  output.df <- output.df[output.df$same==TRUE,]
+  
+}else if(same_pops==FALSE){
+  print("Keeping all population comparisons")
+}
+
+dim(output.df)
+
+## Set up a wrapper to plot all types
+relatedness_metrics <- c("wang", "ritland","quellergt")
+
+# Report
+print("Plotting relatedness, output will be in 03_results")
+
+#### DIFFERENT HERE ###
+
+# Below, had to change to output.dir
+#### END DIFFERENT HERE ###
+
+
+## Loop to plot
+for(i in 1:length(relatedness_metrics)){
+  metric <- relatedness_metrics[i]
+  
+  pdf(file = paste0(output.dir, "relatedness_", metric, "_", date, ".pdf"), width = 20, height = 8)
+  par(mar=c(7,3,3,3))
+  boxplot(output.df[, paste0("rel.", metric)] ~ output.df$group
+          #, col = comp_names.df$colour
+          , las = 2
+          , ylab = paste0("relatedness (", metric, ")")
+  )
+  abline(h = 0, lty = 2)
+  dev.off()
+  
+}
+
+
+
+#### ORIGINAL CODE #####
 
 # Identify which samples will be same-on-same
 comp.names <- unique(output$relatedness$group)
