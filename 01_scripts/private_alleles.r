5f8b5babcdff9737750ea72cf7e76f9a64c7f6a3
bensutherland <bensutherland7@gmail.com>
2020-03-17 09:36:07 -0700

    Finalize code for private alleles

--- 01_scripts/private_alleles.r
+++ 01_scripts/private_alleles.r
@@ -17,12 +17,15 @@ library("hierfstat")
 library("adegenet")
 library("lattice")
 library("gplots")
+#install.packages("poppr")
+library("poppr")
+
 
 # Set working directory for stark or xavier
 # if on a different system, prompt for working directory
 if(Sys.info()["nodename"] == "stark"){ 
   print("On Stark, ready to go")
-  setwd("/mnt/data/01_moore_oyster_project/stacks_workflow/") # stark
+  setwd("/mnt/data/bsuther/01_moore_oyster_project/stacks_workflow/") # stark
 } else if(Sys.info()["nodename"] == "Xavier"){
   print("On Xavier, ready to go")
   setwd("~/Documents/01_moore_oyster_project/stacks_workflow_all_data/") # Xavier
@@ -47,15 +50,9 @@ my_cols.df <- read.csv(file = "../ms_oyster_popgen/00_archive/my_cols.csv", stri
 str(my_cols.df)
 
 #### 02. Private alleles
-library(poppr)
 
 # Tabulate alleles the occur in only one population. 
 private_alleles(gid = my.data.gid)
-
-
-
-###### Here I'm playing around with either removing populations altogether or using the strata argument
-#### I will just re-assign the data
 
 # Do separately for different contrasts
 ## Prepare individual analyses of genind by separating all pops
@@ -63,6 +60,7 @@ sep.obj <- seppop(x = my.data.gid)
 names(sep.obj)
 
 # Create list of various combinations of repooled pops
+# "global" is the comparison of one representative from each main pop
 datatype.list <- list()
 datatype.list[["global"]] <- repool(sep.obj$PEN
                                     , sep.obj$CHN, sep.obj$QDC, sep.obj$RSC
@@ -81,23 +79,25 @@ datatype.list[["bare.minimum"]] <- repool(sep.obj$PEN
 )
 
 
-# Select
+## Select the dataset
+## Main analysis:
+global.gid <- datatype.list[["all"]]
+
+## Test analyses
+#global.gid <- datatype.list[["bare.minimum"]]
 #global.gid <- datatype.list[["global"]]
-global.gid <- datatype.list[["bare.minimum"]]
-#global.gid <- datatype.list[["all"]]
-
-####### END Playing around part, use global.gid
-
 
 ## Create a df that defines the strata for each individual in the rows
 strata.df <- as.data.frame(as.character(pop(global.gid)), stringsAsFactors = FALSE)
 colnames(strata.df)[1] <- "indiv.pop"
 table(strata.df)
 str(strata.df)
-unique(strata.df$repunit)
+# unique(strata.df$repunit)
 
 # Add repunit
 strata.df$repunit <- strata.df$indiv.pop
+
+# Grouping similar pops into the same strata (not all will be present in all datasets, but that is OK)
 strata.df$repunit <- gsub(x = strata.df$repunit, pattern = "CHNF", replacement = "CHN")
 strata.df$repunit <- gsub(x = strata.df$repunit, pattern = "FRAF", replacement = "FRA")
 strata.df$repunit <- gsub(x = strata.df$repunit, pattern = "PENF", replacement = "BC")
@@ -111,8 +111,6 @@ unique(strata.df$repunit)
 # Add strata object to gid
 strata(global.gid) <- strata.df
 table(strata(global.gid))
-
-
 
 ## Characterize sample sizes
 table(strata(global.gid, ~indiv.pop))
@@ -128,17 +126,30 @@ per_pop.privallele <- private_alleles(global.gid, alleles ~ indiv.pop)
 per_repunit.privallele <- private_alleles(global.gid, alleles ~ repunit)
 per_repunit.privallele
 
+# Observe output object
+dim(per_repunit.privallele)
+per_repunit.privallele[,1:5]
+
+# Quantify the number of observations of each private allele in each population
+# e.g.,
+table(per_repunit.privallele["BC",])
+
+# for all repunits
+for(i in 1:nrow(per_repunit.privallele)){
+  
+  print(paste0("The repunit ", rownames(per_repunit.privallele)[i], " has the following observed priv alleles"))
+  print(table(per_repunit.privallele[i, ]))
+  
+}
+
 # Number of observed alleles per locus
-#private_alleles(Pinf, locus ~ Country, count.alleles = TRUE)
-private_alleles(global.gid, count.alleles = TRUE) # I don't know the difference here..
+# private_alleles(Pinf, locus ~ Country, count.alleles = TRUE)
+# private_alleles(global.gid, count.alleles = TRUE) # I don't know the difference here..
 
-# Get raw number of private alleles per locus.
+# Get raw number of private alleles per locus
 pal <- private_alleles(global.gid, locus ~ repunit, count.alleles = FALSE)
-# pal
-# 0   1 
-# 936 117 
-# here the 0 would mean nothing...
-
+table(pal) # this only gives a 0 or 1, does not count alleles, allows one to see exact how many private alleles exist
+# per repunit
 # This shows the number of rows that have a private allele
 rowSums(pal)
 
